84c31d6854583850a03feaa635b64abd8d05d3df
Ivan <ivanakazver3d@gmail.com>
2018-07-22 16:25:04 +0700

    Added welcome message

--- server/server.js
+++ server/server.js
@@ -14,13 +14,30 @@ app.use(express.static(publicPath))
 io.on('connection', socket => {
     console.log('New user connected')
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    })
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'Someone joined the chat',
+        createdAt: new Date().getTime()
+    })
+
     socket.on('createMessage', message => {
         console.log(message)
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         })
+        // socket.broadcast.emit('newMessage', { // отправляет сообщение всем, КРОМЕ того кто отправляет сообщение
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     })
 
     socket.on('disconnect', () => {
