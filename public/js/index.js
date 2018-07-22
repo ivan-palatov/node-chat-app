d0e4c1901cbd8bf8b77e9d48cc1f40ae329e2695
Ivan <ivanakazver3d@gmail.com>
2018-07-22 18:45:23 +0700

    Added geolocation function

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,14 @@ socket.on('newMessage', (message) => {
     $('#messages').append(li)
 })
 
+socket.on('newLocationMessage', message => {
+    let li = $('<li></li>')
+    let a = $('<a target="_blank">My current location</a>')
+    li.text(`${message.from}: `)
+    a.attr('href', message.url)
+    li.append(a)
+    $('#messages').append(li)
+})
 
 $('#message-form').on('submit', e => {
     e.preventDefault()
@@ -21,4 +29,17 @@ $('#message-form').on('submit', e => {
     }, (data) => {
         console.log(data)
     })
+})
+
+let locationButton = $('#send-location')
+locationButton.on('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by Your browser!')
+    }
+    navigator.geolocation.getCurrentPosition(position => {
+        socket.emit('createLocationMessage', {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        })
+    }, () => alert('Unable to fetch location, please try again later.'))
 })
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect')
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
     it('Should generate correct message object', () => {
@@ -7,5 +7,14 @@ describe('generateMessage', () => {
         expect(message.from).toBe('Tester')
         expect(message.text).toBe('Some text')
         expect(typeof message.createdAt).toBe('number')
+    })
+})
+
+describe('generateLocationMessage', () => {
+    it('Should generate correct location object', () => {
+        const message = generateLocationMessage('Tester', 123.123, 241.241)
+        expect(message.from).toBe('Tester')
+        expect(typeof message.createdAt).toBe('number')
+        expect(message.url).toBe('https://www.google.com/maps?q=123.123,241.241')
     })
 })
